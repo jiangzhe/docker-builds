728f31905d330e3e5b4cfee80368fc7d060ae6ec
jiangzhe <zhe.jiang@transwarp.io>
2020-03-12 14:40:22 +0800

    image build of tomcat 8 with openjdk8

---
+++ tomcat-8-openjdk/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# read cgroup memory limit
+CGROUP_MEM_LIMIT=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+# part of cgroup memory limit as max heap size
+JVM_HEAP_PERCENTAGE=${JVM_HEAP_PERCENTAGE:-80}
+# calculate jvm heap
+JVM_HEAP_MB=`expr $CGROUP_MEM_LIMIT / 1024 / 1024 * $JVM_HEAP_PERCENTAGE / 100`
+# max heap size is limited as 30GB
+if (($JVM_HEAP_MB > 30720)); then
+    JVM_HEAP_MB=30720
+fi
+# command-line heap arguments
+JVM_HEAP_ARGS="-Xmx${JVM_HEAP_MB}M"
+# CMS as garbage collector
+JVM_GC_ARGS="-XX:+UseConcMarkSweepGC"
+
+# arguments passed to tomcat server
+export JAVA_OPTS="$JVM_HEAP_ARGS $JVM_GC_ARGS"
+
+# replace current process with tomcat startup
+exec catalina.sh run
+
